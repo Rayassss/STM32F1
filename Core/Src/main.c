217f92469804499e8d6e2aa3f999e5c37a7d05aa
LuoQi <1119060246@qq.com>
2021-01-28 02:04:49 +0800

    delete comments in main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -63,11 +63,6 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
